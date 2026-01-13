14879db1ddf54c70859f062a0c8d874fcb73ef27
Mark Muthii <mark.muthii@strathmore.edu>
2026-01-13 17:07:34 +0300

    Update starter

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,1 +1,10 @@
-// Cache utility
+import NodeCache from "node-cache";
+import { cacheTTL } from "../config/env.js";
+
+const cache = new NodeCache({
+  stdTTL: cacheTTL,
+  checkperiod: cacheTTL * 0.2,
+  useClones: false,
+});
+
+export default cache;
