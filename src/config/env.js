02732fa1ff9ef49900cfacb65874c8438cc0fe29
Mark Muthii <mark.muthii@strathmore.edu>
2026-01-13 17:15:47 +0300

    Update env config

--- src/config/env.js
+++ src/config/env.js
@@ -1,1 +1,6 @@
-// Environment configuration
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const port = process.env.PORT || 3000;
+export const cacheTTL = Number(process.env.CACHE_TTL) || 60;
